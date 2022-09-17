c757a1e5de986118c07865a2e1ae84aa6796847d
Jasmin <jasmin@omerixe.ch>
2022-09-17 10:53:20 +0200

    add location permission to the app as well

--- app/src/main/java/com/hackzurich/hackzurich22/AppNavigation.kt
+++ app/src/main/java/com/hackzurich/hackzurich22/AppNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
     object Welcome : Screen("welcome", R.string.welcome)
