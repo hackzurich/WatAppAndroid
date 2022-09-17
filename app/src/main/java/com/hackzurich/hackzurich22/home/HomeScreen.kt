4f66614cc18a86d834201566d73f8fbe16128786
Jasmin <jasmin@omerixe.ch>
2022-09-17 18:29:56 +0200

    add the design to the homescreen

--- app/src/main/java/com/hackzurich/hackzurich22/home/HomeScreen.kt
+++ app/src/main/java/com/hackzurich/hackzurich22/home/HomeScreen.kt
@@ -1,12 +1,107 @@
 package com.hackzurich.hackzurich22
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hackzurich.hackzurich22.ui.theme.Gray
+import com.hackzurich.hackzurich22.ui.theme.Primary
 
 @Composable
 fun HomeScreen() {
-    Text("Welcome to HackZurich 2022")
+    Column() {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Primary,
+                            fontSize = 36.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append("Wat")
+                    }
+                    withStyle(style = SpanStyle(fontSize = 36.sp, fontWeight = FontWeight.Bold)) {
+                        append("App")
+                    }
+                }
+            )
+
+            Icon(
+                painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                tint = Primary,
+                modifier = Modifier.size(60.dp)
+            )
+        }
+        Text(buildAnnotatedString {
+            withStyle(style = SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)) {
+                append("You saved ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = Primary,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("20 000 litres\n")
+            }
+            withStyle(style = SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)) {
+                append("Which can provide water to  ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = Primary,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("123.5 people ")
+            }
+            withStyle(style = SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)) {
+                append("in Switzerland ")
+            }
+        }, modifier = Modifier.padding(16.dp))
+        Row(modifier = Modifier.padding(horizontal = 16.dp, vertical = 0.dp)) {
+            Icon(
+                painterResource(id = R.drawable.ic_person),
+                contentDescription = null,
+                tint = Primary
+            )
+            (0..16).forEach {
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    painterResource(id = R.drawable.ic_person),
+                    contentDescription = null,
+                    tint = Gray
+                )
+            }
+        }
+        Text(
+            "0,00014% of population",
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            style = TextStyle(color = Gray)
+        )
+    }
 }
 
 @Preview
--- app/src/main/java/com/hackzurich/hackzurich22/ui/theme/Color.kt
+++ app/src/main/java/com/hackzurich/hackzurich22/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.hackzurich.hackzurich22.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Primary = Color(0xFF377EA6)
-val Purple500 = Color(0xFF6200EE)
+val Gray = Color(0xFF979797)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
