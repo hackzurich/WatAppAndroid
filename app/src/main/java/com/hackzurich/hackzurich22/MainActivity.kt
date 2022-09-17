967d94712fbb64c6e26752d36c6e4a30b5f73e03
Jasmin <jasmin@omerixe.ch>
2022-09-17 13:04:59 +0200

    add a nice splash screen and change the colors

--- app/src/main/java/com/hackzurich/hackzurich22/MainActivity.kt
+++ app/src/main/java/com/hackzurich/hackzurich22/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hackzurich.hackzurich22
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             App()
         }
