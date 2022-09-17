9190fefa34681f57665f8b558351b4a0338b1c9e
Jasmin <jasmin@omerixe.ch>
2022-09-17 02:18:13 +0200

    add hilt to the app as well

--- app/src/main/java/com/hackzurich/hackzurich22/MainActivity.kt
+++ app/src/main/java/com/hackzurich/hackzurich22/MainActivity.kt
@@ -3,25 +3,11 @@ package com.hackzurich.hackzurich22
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
-import com.hackzurich.hackzurich22.ui.theme.HackZurich22Theme
+import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalPermissionsApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/hackzurich/hackzurich22/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.hackzurich.hackzurich22
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
